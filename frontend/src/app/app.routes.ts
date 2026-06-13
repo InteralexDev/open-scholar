fde059aa168c7ab21973712ef8bad0b8664f88dc
InteralexDev <interalex1995@gmail.com>
2026-06-13 14:08:07 +0200

    feat: add application routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'publications/search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'publications/search',
+    loadComponent: () =>
+      import('./features/publications/pages/search/search.page')
+        .then(m => m.SearchPage)
+  },
+  {
+    path: 'publications/:openAlexId',
+    loadComponent: () =>
+      import('./features/publications/pages/details/details.page')
+        .then(m => m.DetailsPage)
+  },
+  {
+    path: '**',
+    redirectTo: 'publications/search'
+  }
+];
